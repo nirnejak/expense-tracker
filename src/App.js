372fe34afa9c01e53b6f8240094534c93875a29f
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-03-31 00:19:37 +0530

    Components and UI Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import Layout from './components/Layout'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
+
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Layout>
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+    </Layout>
   );
 }
 
